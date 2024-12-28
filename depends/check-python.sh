8a0fac6b3db2442312da8addfec4ece453623017
Aislynn Ortega <aislynnortega@riseup.net>
2024-12-28 18:44:33 -0300

    Use Python3 by default for check-python
    Python2 has been deprecated already, let's use Python3
    
    Signed-off-by: Aislynn Ortega <aislynnortega@riseup.net>

--- depends/check-python.sh
+++ depends/check-python.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-# check-python.sh by Naomi Peori (naomi@peori.ca)
+# check-python3.sh by Naomi Peori (naomi@peori.ca)
 
-## Check for python.
-( python --version || python -V ) 1>/dev/null 2>&1 || { echo "ERROR: Install python before continuing."; exit 1; }
+## Check for python3.
+( python3 --version || python3 -V ) 1>/dev/null 2>&1 || { echo "ERROR: Install python3 before continuing."; exit 1; }
 
-## Check for python-config
-pyprefix=$(python-config --prefix)
-[ $? -eq 0 ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
+## Check for python3-config
+pyprefix=$(python3-config --prefix)
+[ $? -eq 0 ] || { echo "ERROR: Install python3-dev before continuing."; exit 1; }
 
-## Check for python header files
-( ls -1d "${pyprefix}"/include/python[23].*/Python.h || ls -1d /opt/local/include/python[23].*/Python.h ) 1>/dev/null 2>&1 || [ -f "$PYINSTALLDIR/include/Python.h" ] || { echo "ERROR: Install python-dev before continuing."; exit 1; }
+## Check for python3 header files
+( ls -1d "${pyprefix}"/include/python3.*/Python.h || \
+  ls -1d /opt/local/include/python3.*/Python.h ) 1>/dev/null 2>&1 || \
+    [ -f "$PYINSTALLDIR/include/Python3.h" ] || \
+    { echo "ERROR: Install python3-dev before continuing."; exit 1; }
